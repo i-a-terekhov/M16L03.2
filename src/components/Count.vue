0209cb4ac525419fabdb08acf4982a1ce34fef7e
Terekhov Ivan <for-spam32@yandex.ru>
2026-09-08 01:56:37 +0400

    translate previous code in current project

---
+++ src/components/Count.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+const count = ref(3);
+
+function changeCount() {
+  count.value++;
+}
+
+onMounted(() => {
+  console.log('Component mounted. Count is ' + count.value);
+});
+</script>
+
+<template>
+  <p>Count is {{ count }}</p>
+  <button @click="changeCount">Click me</button>
+</template>
+
+<style scoped>
+</style>
