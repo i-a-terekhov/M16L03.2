5bbbdfd8877fad5914bc29224fe4b7447085b91e
Terekhov Ivan <for-spam32@yandex.ru>
2026-09-08 18:05:52 +0400

    using value from input

--- src/components/Count.vue
+++ src/components/Count.vue
@@ -1,15 +1,27 @@
 <script setup>
+import { ref } from 'vue';
+
 const props = defineProps(["number", "count"]);
 const emit = defineEmits(["onCountChange"]);
 
+const data = ref(0);
+
 function changeCount() {
-  emit('onCountChange', props.count + 1);
+  const intData = parseInt(data.value);
+  emit('onCountChange', props.count + (!isNaN(intData) ? intData : 1));
+}
+
+function changeData(event) {
+  data.value = event.target.value;
 }
 </script>
 
 <template>
+  <div>
     <p>Count is {{ count }}</p>
+    <input type="text" :value="data" @input="changeData"/>
     <button @click="changeCount">Click me</button>
+  </div>
 </template>
 
 <style scoped>
